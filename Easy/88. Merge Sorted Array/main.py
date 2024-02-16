b4363f33a60b7cc1547a22d100323b7212d6573b
Pavel Budkov <pklerik@mail.ru>
2024-02-16 14:23:39 +0300

    [new] - O(n+m)

--- Easy/88. Merge Sorted Array/main.py
+++ Easy/88. Merge Sorted Array/main.py
@@ -10,19 +10,23 @@ class Solution:
             return
         for _ in range(m + n):
             if not nums2:
+                if nums1[pointer] == 0:
+                    nums1.pop(pointer)
                 continue
             if nums1[pointer] > nums2[0]:
                 nums1.insert(pointer, nums2.pop(0))
+                nums1.pop(-1)
                 pointer += 1
             elif nums1[pointer] == 0:
                 if len(nums2) != 0 and pointer >= n + m - len(nums2):
                     nums1.insert(pointer, nums2.pop(0))
+                    nums1.pop(-1)
                     pointer += 1
                 else: pointer += 1
             else: pointer += 1
 
-        for _ in range(n):
-            nums1.pop(-1)
+        # for _ in range(n):
+        #     nums1.pop(-1)
 
         # nums1 = nums1[:-n]
 
